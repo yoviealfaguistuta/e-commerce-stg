425b33f3c5bbb800087d1436c96917155b68418f
Fadil Ai <ainuddinalaskhary@gmail.com>
2023-03-13 12:46:47 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/css/style.css';
-import './assets/css/plugins.css';
-import './assets/css/custom.css';
 
-// import './App.css';
+
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
