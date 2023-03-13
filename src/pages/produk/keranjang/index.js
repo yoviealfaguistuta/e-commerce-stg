9c482deac5297f1f312a692af89bfa074db03162
Fadil Ai <ainuddinalaskhary@gmail.com>
2023-03-13 10:50:25 +0700

    folder

---
+++ src/pages/produk/keranjang/index.js
@@ -0,0 +1,19 @@
+import React, { Fragment, useState } from 'react'
+import { Layout } from '../../../layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Keranjang = () => {
+
+  
+
+  return (
+    <Layout>
+      <div>
+       
+      </div>
+    </Layout>
+  );
+};
+
+export default Keranjang;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import {
 } from "react-router-dom";
 import Home from "../pages/home";
 import List from "../pages/produk/list";
+import Keranjang from "../pages/produk/keranjang";
+import Profile from "../pages/produk/profile";
+
+
 import About from "../pages/produk/list/about";
 import Contact from "../pages/produk/list/contact";
 import Detail from "../pages/produk/detail";
@@ -25,6 +29,15 @@ const router = createBrowserRouter([
         path: "/detail/:id_barang",
         element: <Detail />,
     },
+    {
+        path: "/keranjang",
+        element: <Keranjang />,
+    },
+    {
+        path: "/profile",
+        element: <Profile />,
+    },
+    
     {
         path: "/about",
         element: <About />,
